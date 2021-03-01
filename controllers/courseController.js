1e149fe7cc00663aefb980854ab8249a96fb5e92
Ryosukekamimura <come1234560@gmail.com>
2021-03-01 15:39:50 +0900

    feat: Create Controller

---
+++ controllers/courseController.js
@@ -0,0 +1,45 @@
+const { body, validationResult } = require('express-validator')
+var async = require('async')
+
+// GET ~/early 前期授業一覧を取得する
+
+exports.early_course_list = (req, res, next) => {
+  res.json({
+    "route" : "early"
+  })
+}
+
+// GET ~/early/:id 前期授業の詳細を取得する
+exports.early_course_details = (req, res, next) => {
+  res.json({
+    "route" : ":id"
+  })
+}
+
+// GET ~/latter 後期授業一覧を取得する
+exports.latter_course_list = (req, res, next) => {
+  res.json({
+    "route" : "latter"
+  })
+}
+
+// GET ~/latter/:id 後期授業の詳細を取得する
+exports.latter_course_details = (req, res, next) => {
+  res.json({ 
+    "route" : ":id"
+  })
+}
+
+// GET ~/comments/:id コメントの詳細を取得する
+exports.comments_details = (req, res, next) => {
+  res.json({
+    "route" : "comments_details"
+  })
+}
+
+// POST ~/comments/create コメントを作成する
+exports.comments_create = (req, res, next) => {
+  res.json({
+    "route" : "comments_create"
+  })
+}
